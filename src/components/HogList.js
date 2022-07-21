5308c6ae4b40b44be5737e0fe079fa5e4fbbd0f5
Andrea Rosentel <andrearosentel@gmail.com>
2022-07-21 08:40:33 -0700

    the sort works!

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -11,15 +11,49 @@ const HogList = ({piggies}) => {
         setSort(e.target.value)
     }
 
-    console.log(piggies.sort(function(a,b){
-        if(a.name<b.name){
-            return -1
+    function handleSortPigs(sortPigs, piggies) {
+        if (sortPigs === 'A') {
+            return piggies.sort(function(a,b){
+                if(a.name<b.name){
+                    return -1
+                }
+                if(a.name>b.name){
+                    return 1
+                }
+                return 0;
+            })
+        } else if (sortPigs === 'Z') {
+            return piggies.sort(function(a,b){
+                if(a.name>b.name){
+                    return -1
+                }
+                if(a.name<b.name){
+                    return 1
+                }
+                return 0;
+            })
+        } else if (sortPigs === 'light') {
+            return piggies.sort(function(a,b){
+                if(a.weight<b.weight){
+                    return -1
+                }
+                if(a.weight>b.weight){
+                    return 1
+                }
+                return 0;
+            })
+        } else if (sortPigs === 'heavy') {
+            return piggies.sort(function(a,b){
+                if(a.weight>b.weight){
+                    return -1
+                }
+                if(a.weight<b.weight){
+                    return 1
+                }
+                return 0;
+            })
         }
-        if(a.name>b.name){
-            return 1
-        }
-        return 0;
-    }))
+    }
 
 
     function handleChange(e) {
@@ -42,7 +76,7 @@ const HogList = ({piggies}) => {
             <Filter 
             onSortChange = {handleSort}
             onCategoryChange={handleChange}/>
-            <ul className="hogList">{hogsToDisplay.map((piggie) => <HogTile key = {piggie.name} name={piggie.name} image={piggie.image} specialty={piggie.specialty} weight={piggie.weight} greased={piggie.greased} medal={piggie["highest medal achieved"]} />)}
+            <ul className="hogList">{handleSortPigs(sortPigs, hogsToDisplay).map((piggie) => <HogTile key = {piggie.name} name={piggie.name} image={piggie.image} specialty={piggie.specialty} weight={piggie.weight} greased={piggie.greased} medal={piggie["highest medal achieved"]} />)}
             </ul>
         </div>
     )
